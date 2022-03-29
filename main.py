fc0d2a7496494e6c8e805c6fd858a7fe665f5dfb
KShiMin <KShiMin@github.com>
2022-03-29 20:37:27 +0800

    updated as of 29/03/2020 @ 20:37

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__': # only allow web server to run if this file is ran di
     # going to run the web server
     # debug = true --> anytime python code is ran, we server is automatically re-run
     # host = '0.0.0.0' allow external visibility 
-    app.run(debug=True, host='0.0.0.0', port="8008")
+    app.run(debug=True, host='0.0.0.0', port="8080")
